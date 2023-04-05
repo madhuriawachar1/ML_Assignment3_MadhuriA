fa0a5b4c15564eb1bbb7894289bf67e744bd8006
madhuriawachar1 <awacharmadhuri@iitgna.c.in>
2023-04-05 16:10:40 +0530

    Initial1 commit

--- g.py
+++ g.py
@@ -19,7 +19,7 @@ def plot_decision_boundary(problem, n_estimators):
         X, y = make_regression(n_samples=500, n_features=1, noise=10, random_state=42)
         model = BaggingRegressor(RandomForestRegressor(n_estimators=1, max_depth=2), n_estimators=n_estimators, random_state=42)
         error = mean_squared_error
-    
+    #vnvmbk
     # Train the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model.fit(X_train, y_train)
