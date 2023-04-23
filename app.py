c033d8854a3cc5b7f8cd2ce9944525505ee3dbad
madhuriawachar1 <awacharmadhuri@iitgna.c.in>
2023-04-23 19:48:38 +0530

    readme updated

--- app.py
+++ app.py
@@ -25,3 +25,69 @@ model.fit(X_train, y_train)
 st.write('Selected task:', option)
 st.write('Number of learners:', n_learners)'''
 
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_classification, make_regression
+from sklearn.ensemble import BaggingClassifier, BaggingRegressor
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from mlxtend.plotting import plot_decision_regions
+
+# Define a function to create the data
+def create_data(option):
+    if option == 'Classification':
+        X, y = make_classification(n_samples=200, n_features=2, n_informative=2,
+                                    n_redundant=0, n_clusters_per_class=1, random_state=42)
+    else:
+        X, y = make_regression(n_samples=200, n_features=1, noise=20, random_state=42)
+    return X, y
+
+# Define a function to create the bagging model
+def create_model(option, n_learners):
+    if option == 'Classification':
+        base_estimator = DecisionTreeClassifier()
+        model = BaggingClassifier(base_estimator=base_estimator, n_estimators=n_learners)
+    else:
+        base_estimator = DecisionTreeRegressor()
+        model = BaggingRegressor(base_estimator=base_estimator, n_estimators=n_learners)
+    return model
+
+# Define the Streamlit app
+st.title('Bagging Demo')
+
+# Define a dropdown menu to select classification or regression
+option = st.sidebar.selectbox('Select a task:', ('Classification', 'Regression'))
+
+# Define a slider to adjust the number of learners
+n_learners = st.sidebar.slider('Number of learners:', 1, 50, 10)
+
+# Create the data and the bagging model
+X, y = create_data(option)
+model = create_model(option, n_learners)
+
+# Fit the model to the data
+model.fit(X, y)
+
+# Compute the decision boundary for the bagging model
+if option == 'Classification':
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                         np.arange(y_min, y_max, 0.02))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+else:
+    xx = np.arange(X.min(), X.max(), 0.1).reshape(-1, 1)
+    Z = model.predict(xx)
+
+# Visualize the data and the decision boundary
+fig, ax = plt.subplots()
+if option == 'Classification':
+    plot_decision_regions(X, y, model, ax=ax)
+else:
+    ax.plot(X, y, 'o', label='data')
+    ax.plot(xx, Z, label='bagging')
+ax.set_xlabel('X')
+ax.set_ylabel('y')
+ax.legend()
+st.pyplot(fig)
--- help.md
+++ help.md
@@ -1,4 +1,38 @@
-help to resolve tensorflow related issues in mac air m1
+# Introduction
 
-``https://www.youtube.com/watch?v=6W8pjnW65Q8``
+Bagging can reduce variance in model because of bootstraping and aggregation without impacting the bias.
+in bagging each model is low bias and high variance model.
+
+
+Bagging is a technique that involves training multiple models on different subsets of the training data and then aggregating their predictions to obtain a final prediction. 
+
+By using thesemultiple models trained, bagging reduces the variance of the overall model, which can improve its generalization performance on new, unseen data.
+
+
+* create model
+
+In the above code, ```the create_model function ```creates a bagging model for either classification or regression.
+
+ The function takes an argument ```n_estimators```, which determines the number of learners to use in the bagging model. The more learners there are in the bagging model, the lower the variance of the overall model is likely to be.
+
+To demonstrate how bagging reduces variance, the Streamlit app allows the user to adjust the number of learners using a slider in the sidebar menu. As the number of learners increases, the variance of the model is expected to decrease, as the aggregate prediction becomes more robust to the idiosyncrasies of any individual model.
+
+In the app, the decision boundary visualizations show how the bagging model changes as the number of learners is increased. 
+
+### For classification
+the decision boundary becomes smoother and less sensitive to small changes in the input features as the number of learners increases.
+
+### For regression
+ the prediction becomes smoother and less erratic as the number of learners increases, as the aggregate prediction becomes less sensitive to the specific training data used to fit each individual model.
+
+Overall, the app demonstrates how bagging can reduce variance for both classification and regression, and how this is demonstrated by the number of learners used in the bagging model.
+
+
+
+
+
+
+
+
+
 
